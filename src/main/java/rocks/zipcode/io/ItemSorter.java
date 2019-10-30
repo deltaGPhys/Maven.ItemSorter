9ee0941ad6f3121d1cfbf52d263266b1d8c58bf9
deltagphys <cty.jgates@gmail.com>
2019-10-30 16:30:13 -0400

    done - this is a super cool tool

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,23 @@
 package rocks.zipcode.io;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+    private Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(this.items, comparator);
+        return this.items;
     }
 }
